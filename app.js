724a02824a2d9b2d2cc55eb6b68c7454a5e89284
Katie Eells <katieeells@Katies-MBP.lan>
2022-04-16 11:57:42 -0400

    added functionality to key buttons

--- app.js
+++ app.js
@@ -22,6 +22,28 @@ window.onload = function(){
 let currentRow = 0;
 let currentTile = 0;
 
-// Event listener for 
+// Event listener for key clicks
+
+// this eventlistener works in this code but does not specify the letter of the key
+// document.addEventListener('click', (e) => {
+//     // console.log('clicked')  <-- this console.log is just a test that worked  
+// })
+
+
+// this code does not do anything in console
+// const keyHandle = document.querySelectorAll(".key-row button");
+
+// for (let i = 0; i < keyHandle.length; i++) {
+//     keyHandle[i].onclick = ({target}) => {
+//         const keyLetter = target.getAttribute("data-key");
+
+//         console.log(keyLetter)
+//     }
+// }
+
+function keyHandle(key) {
+    var keyLetter = key.getAttribute("data-key");
+    console.log(key.innerHTML);
+  }
 
 let gameOver = false;
